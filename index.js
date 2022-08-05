f3ca139a7395d9baa2e03baf456b2be750c00089
anuprajvarma <anupraj1854@gmail.com>
2022-08-05 16:37:23 +0530

    Add post request feature in index.js

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express')
+const fs = require('fs')
+const youtubedl = require('youtube-dl')
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send("Hii there ")
+})
+
+app.post('/search', (req, res) => {
+    const video = youtubedl('http://www.youtube.com/watch?v=90AiXO1pAiA')
+
+    video.on('info', function (info) {
+        console.log('Download started')
+        console.log('filename: ' + info._filename)
+        console.log('size: ' + info.size)
+    })
+
+    video.pipe(fs.createWriteStream('myvideo.mp4'))
 })
 
 app.listen(5400, () => {
