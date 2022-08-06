9b1712bf2d89fde5896d569a99a1cdaa7c9fd091
anuprajvarma <anupraj1854@gmail.com>
2022-08-06 16:54:32 +0530

    Deploy on heroku

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const youtubedl = require('youtube-dl-exec');
+const path = require('path')
+require('dotenv').config();
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+const PORT = process.env.PORT;
 
 app.post('/search', (req, res) => {
     const link = req.body.url;
@@ -18,6 +22,26 @@ app.post('/search', (req, res) => {
     video.stderr.pipe(stream);
 })
 
-app.listen(5400, () => {
-    console.log("server is started on 5400")
+// --------- deployment ---------- 
+
+
+__dirname = path.resolve();
+if (process.env.NODE_ENV = 'production') {
+    app.use(express.static(path.join(__dirname, "/client/build")));
+
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+
+} else {
+    app.get('/', (req, res) => {
+        res.send("API is running ")
+    })
+}
+
+// --------- deployment ---------- 
+
+app.listen(PORT, () => {
+    console.log(`server is started on ${PORT}`)
 })
